e24bb53b3b737e4afa35cfb9ab8e14faf5d14d2d
Nicholas Stone <nstone008@gmail.com>
2022-06-20 22:56:41 -0500

    Have created a basic response
    Using the options I need to process the data next

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,6 @@
 const { MessageActionRow, MessageButton } = require('discord.js');
-const { getCommandFiles } = require('../utils')
+const { getCommandFiles } = require('../utils');
+const wait = require('node:timers/promises').setTimeout;
 
 // Currently this is the file that responds to interactions(Slash commands is the only version i know)
 
@@ -8,23 +9,35 @@ module.exports = {
     async execute(interaction){
         console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
 
-        if (!interaction.isCommand()) return;
+        if (interaction.isCommand()){
 
-        const commandsToCheck = getCommandFiles();
+            const commandsToCheck = getCommandFiles();
 
-        //-- Dynamic Command Execution
-        const command = commandsToCheck.find(element => element.data.name === interaction.commandName);
-        
-        if (!command){
-            return await interaction.reply(`Can't find the command ${interaction.commandName}`)
-        };
+            //-- Dynamic Command Execution
+            const command = commandsToCheck.find(element => element.data.name === interaction.commandName);
+            
+            if (!command){
+                return await interaction.reply(`Can't find the command ${interaction.commandName}`)
+            };
 
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(error);
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+            //--
         }
-        //--
+        else if(interaction.isSelectMenu()) {
+
+            if (interaction.customId === 'select') {
+                
+                
+                await interaction.deferUpdate();
+                await wait(2000);                
+                await interaction.editReply({ content: 'Something was selected!', components: [] });
+            }
+
+        }
      }
 }
